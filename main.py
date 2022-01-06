21f896e162f1dc101f9c631deba4315d16589e2b
Jonathan Ogboru <62063539+jojogamezHub@users.noreply.github.com>
2022-01-06 01:25:34 +0000

    Add more packets

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("TheBeatdown","Beatdown.mp4",.1,.1,10)
+    encoder.encode("TheBeatdown","Beatdown.mp4",.1,1,10)
 
 if __name__== "__main__":
     main()
