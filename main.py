1dc98aa1804a60e3dbd9bd29ca1c411bbdec4a65
Jonathan Ogboru <62063539+jojogamezHub@users.noreply.github.com>
2022-01-06 02:09:03 +0000

    New Movie!!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("TheBeatdown","Beatdown.mp4",.1,1,10)
+    encoder.encode("Coffee","Coffee.mp4",.3,0.5,10)
 
 if __name__== "__main__":
     main()
