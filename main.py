6d7a59f90ff3d28ab5b01d57e6c10765fc3d8cb1
Jonathan Ogboru <62063539+jojogamezHub@users.noreply.github.com>
2022-01-08 09:05:35 +0000

    spiderman:homecoming trailer video added.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import encoder
 
 
 def main():
-    encoder.encode("Coffee", "Coffee.mp4", .125, 0.86, 16)
+    encoder.encode("SpiderMan", "SpiderMan.mp4", .125, 0.65, 16)
 
 
 if __name__ == "__main__":
